Fix aggregated_jaccard Add normalization layer to DoubleUNet

models/unet/double_unet_model.py
@@ -1,4 +1,4 @@
-from common import *
+from .unet_parts import *
 from ..unet import UNet
 
 
@@ -16,15 +16,17 @@ class DoubleUNet(nn.Module):
             config2['in_channels'] += config1['up'][-1][0]
         self.unet1 = UNet(config1)
         self.unet2 = UNet(config2)
+        self.l2_norm = normalize()
 
     def forward(self, x):
         if self.concat == 'input':
-            output1 = self.unet1(x)
+            output1 = self.l2_norm(self.unet1(x))
             x = torch.cat([x, output1], dim=1)
         elif self.concat == 'penultimate':
             penultimate, output1 = self.unet1(x)
+            output1 = self.l2_norm(output1)
             x = torch.cat([penultimate, output1], dim=1)
         else:
-            x = output1 = self.unet1(x)
+            x = output1 = self.l2_norm(self.unet1(x))
         output2 = self.unet2(x)
         return output1, output2

predict_double_unet.py
@@ -41,9 +41,9 @@ def post_processing_randomwalk(pred, dilation=None):
 ########################### Config Predict ##############################
 net = DoubleWiredUNet(DOUBLE_UNET_CONFIG_1).cuda()
 
-weight_path = os.path.join(WEIGHTS_DIR, 'final/dwunet3_6_1e-03_491.6902.pth')
+weight_path = os.path.join(WEIGHTS_DIR, 'double-wired-unet-0.4495.pth')
 net.load_state_dict(torch.load(weight_path))
-output_path = 'DWUNET13'
+output_path = 'DWUNET4'
 output_path = os.path.join(OUTPUT_DIR, output_path)
 if not os.path.exists(output_path):
     os.makedirs(output_path)
@@ -85,7 +85,7 @@ for test_id in TEST_IDS:
     # cv2.waitKey(0)
 
     labels_path = os.path.join(INPUT_DIR, LABELS_DIR, test_id+'.npy')
-    gt_labels = np.load(labels_path).astype(np.uint8)
+    gt_labels = np.load(labels_path).astype(np.int)
     agg_jac = aggregated_jaccard(pred_labels, gt_labels)
     sum_agg_jac += agg_jac
     print('{}\'s Aggregated Jaccard Index: {:.4f}'.format(test_id, agg_jac))

utils/metrics.py
@@ -8,7 +8,7 @@ def criterion_MSELoss(logits, labels):
 def criterion_AngularError(logits, labels, areas):
     eps = 10 ** -6
     weights = torch.gt(areas, 0).float() / torch.sqrt(areas+eps)
-    weights = weights / (weights.max()+eps)
+    # weights = weights / (weights.max()+eps)
     # weights = areas
     loss = AngularErrorLoss()(logits[:, :2], labels[:, :2], weights) + \
            AngularErrorLoss()(logits[:, 2:], labels[:, 2:], weights)
@@ -90,11 +90,6 @@ class AngularErrorLoss(nn.Module):
         super(AngularErrorLoss, self).__init__()
 
     def forward(self, logits, labels, weights):
-        # probs = F.tanh(logits)
-        # probs = F.sigmoid(logits)
-        # norm_logits = F.normalize(logits, p=2, dim=1) * 0.999999  # multiplying by 0.999999 prevents 'nan'!
-        # norm_labels = F.normalize(labels, p=2, dim=1) * 0.999999  # multiplying by 0.999999 prevents 'nan'!
-
         dot_prods = torch.sum(logits * labels, 1)
         dot_prods = dot_prods.clamp(-1, 1)
         error_angles = torch.acos(dot_prods)
@@ -167,31 +162,56 @@ def dice_index(pred_mask, gt_mask):
 def aggregated_jaccard(pred_labels, gt_labels):
     C = 0
     U = 0
-    pred_ls = list(range(1, pred_labels.max()+1))
-    for gt_l in range(1, gt_labels.max()+1):
+    for gt_l in [l for l in np.unique(gt_labels) if l != 0]:
         gt_label = (gt_labels == gt_l).astype(np.int)
-        max_iou = 0
         meeting_pred_ls = [p for p in np.unique(pred_labels * gt_label) if p != 0]
         if len(meeting_pred_ls) == 0:
             U += gt_label.sum()
+        max_iou = 0
         for pred_l in meeting_pred_ls:
-            if pred_l not in pred_ls:
-                continue
             pred_label = (pred_labels == pred_l).astype(np.int)
             intersection = (pred_label * gt_label).sum()
             union = pred_label.sum() + gt_label.sum() - intersection
-            eps = 10**-6
-            iou = intersection / (union+eps)
-            if (iou > max_iou):
+            iou = intersection / union
+            if iou > max_iou:
                 max_iou = iou
-                m_l = pred_l
+                m_label = pred_label
                 m_intersection = intersection
                 m_union = union
         if max_iou != 0:
             C += m_intersection
             U += m_union
-            pred_ls.remove(m_l)
-    for pred_l in pred_ls:
-        pred_label = (pred_labels == pred_l).astype(np.int)
-        U += pred_label.sum()
-    return C / U if U != 0 else 0
+            pred_labels *= 1 - m_label
+    U += (pred_labels > 0).sum()
+    return C / U
+
+
+# def aggregated_jaccard(pred_labels, gt_labels):
+#     C = 0
+#     U = 0
+#     for gt_l in [l for l in np.unique(gt_labels) if l != 0]:
+#         gt_label = (gt_labels == gt_l).astype(np.int)
+#         meeting_pred_ls = [p for p in np.unique(pred_labels * gt_label) if p != 0]
+#         max_iou = 0
+#         if len(meeting_pred_ls) == 0:
+#             m_label = (pred_labels == 1).astype(np.int)
+#             m_intersection = 0
+#             m_union = m_label.sum() + gt_label.sum()
+#         for pred_l in meeting_pred_ls:
+#             pred_label = (pred_labels == pred_l).astype(np.int)
+#             intersection = (pred_label * gt_label).sum()
+#             union = pred_label.sum() + gt_label.sum() - intersection
+#             iou = intersection / union
+#             if iou > max_iou:
+#                 max_iou = iou
+#                 m_label = pred_label
+#                 m_intersection = intersection
+#                 m_union = union
+#         C += m_intersection
+#         U += m_union
+#         pred_labels *= 1 - m_label
+#     U += (pred_labels > 0).sum()
+#     return C / U
+
+
+