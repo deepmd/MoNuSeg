Fix unet1 output masking and add BN to unet2 input

models/unet/double_unet_model.py
@@ -8,8 +8,8 @@ class DoubleUNet(nn.Module):
         config1 = config['unet1']
         config2 = config['unet2']
         self.concat = config.get('concat', None)
-        config2['in_channels'] = config1['out_channels']
-        if self.concat == 'input':
+        config2['in_channels'] = config1['out_channels'] if self.concat != 'input-discard_out1' else 0
+        if self.concat == 'input' or self.concat == 'input-discard_out1':
             config2['in_channels'] += config1['in_channels']
         elif self.concat == 'penultimate':
             config1['penultimate_output'] = True
@@ -19,7 +19,9 @@ class DoubleUNet(nn.Module):
         self.l2_norm = normalize()
 
     def forward(self, x):
-        if self.concat == 'input':
+        if self.concat == 'input-discard_out1':
+            output1 = self.l2_norm(self.unet1(x))
+        elif self.concat == 'input':
             output1 = self.l2_norm(self.unet1(x))
             x = torch.cat([x, output1], dim=1)
         elif self.concat == 'penultimate':
@@ -28,5 +30,6 @@ class DoubleUNet(nn.Module):
             x = torch.cat([penultimate, output1], dim=1)
         else:
             x = output1 = self.l2_norm(self.unet1(x))
+        x = self.bn(x)
         output2 = self.unet2(x)
         return output1, output2

models/unet/double_wired_unet_model.py
@@ -9,11 +9,12 @@ class DoubleWiredUNet(DoubleUNet):
             raise ValueError('Length of \'down\' of both UNets should be the same.')
         self.unet2 = UNet2(config['unet2'], config['unet1'])
         self.l2_norm = normalize()
+        self.bn = nn.BatchNorm2d(config['unet2']['in_channels'])
 
     def forward(self, x, mask=None):
         unet1_d_outs = []
         unet2_d_outs = []
-        inp = x if self.concat == 'input' else None
+        inp = x if self.concat == 'input' or self.concat == 'input-discard_out1' else None
 
         # UNet1
         for down in self.unet1.downs:
@@ -26,12 +27,20 @@ class DoubleWiredUNet(DoubleUNet):
         output1 = self.unet1.outc(x)
         output1 = self.l2_norm(output1)
 
-        if self.concat == 'input':
+        if mask is not None:
+            mask = torch.unsqueeze(mask, dim=1)
+            mask = mask.repeat((1, output1.shape[1], 1, 1))
+
+        if self.concat == 'input-discard_out1':
+            x = inp
+        elif self.concat == 'input':
             x = torch.cat([inp, output1], dim=1) if mask is None else torch.cat([inp, output1*mask], dim=1)
         elif self.concat == 'penultimate':
             x = torch.cat([x, output1], dim=1) if mask is None else torch.cat([x, output1*mask], dim=1)
         else:
             x = output1 if mask is None else output1*mask
+
+        x = self.bn(x)
 
         # UNet2
         for down, d_out1 in zip(self.unet2.downs, unet1_d_outs):